2d55791f5961affcce39bd5e071cf13dff1ad9f3
Pedro Machado Leiva <pyuris22@gmail.com>
2022-12-04 19:37:26 -0500

    updated marketplace

--- contracts/BlockitusMarketplace.sol
+++ contracts/BlockitusMarketplace.sol
@@ -3,52 +3,74 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 
-import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 
 contract BlockitusMarketplace {
-    //uint8 constant fee = 1;
+    uint8 constant fee = 1;
     address public owner;
 
 
-    event PutOffer(uint256 id, address indexed seller, uint256 price);
-    event GetOffer(uint256 id , address indexed buyer, uint256 price);
+    event Sell(address indexed seller, uint256 id, uint256 price);
+    event Buy(address indexed buyer, address indexed seller, uint256 id, uint256 price);
 
     IERC721 nft_collection;
 
-    mapping (address => mapping (uint256 => uint256)) prices;
+    mapping (address => mapping (uint256 => uint256)) private _offers;
 
     constructor () {
         owner = msg.sender;
     }
 
     function sell(address collection, uint256 id, uint256 price) public {
-        //approve from web3 first at all.
-        require(price > 0, "Notifcation: The price should be greater than 0.");
-        prices[collection][id] = price;
-        emit PutOffer(id, msg.sender, price);
+        //owner should have to approve the contract before call this function
+        nft_collection = IERC721(collection);
+        require(nft_collection.ownerOf(id) == msg.sender || nft_collection.getApproved(id) == msg.sender, "Marketplace: You have not permits over this item");
+        require(price > 0, "Marketplace: The price should be greater than 0.");
+        _offers[collection][id] = price;
+        emit Sell(msg.sender, id,  price);
         
     }
 
 
     function buy(address collection, uint256 id) public payable  {
-        nft_collection = IERC721(collection);
-        require(msg.value > 0, "Notification: Error in amount");
-        require(msg.value >= prices[collection][id], "Notification: You need to paid the correct price");
-        address _owner = nft_collection.ownerOf(id);
-        nft_collection.safeTransferFrom(_owner, msg.sender, id);
-        delete prices[collection][id];
-        payable(_owner).transfer(msg.value);
-        emit GetOffer(id, msg.sender, prices[collection][id]);
+        nft_collection = IERC721(collection); 
+        address seller = nft_collection.ownerOf(id);
+        uint256 price = _offers[collection][id];
+        uint256 net = net_pay(collection, id);
+        uint256 remaining = msg.value - net;
+        require(msg.value > 0 && msg.value >= net , "Marketplace: Error in amount");
+        require(msg.sender != seller, "Marketplace: You are the NFT's owner"); //msg.sender is the buyer
+        nft_collection.safeTransferFrom(seller, msg.sender, id);
+        delete _offers[collection][id];
+        payable(seller).transfer(price);
+        if (remaining > 0) {
+            payable(msg.sender).transfer(remaining);
+        }
+        emit Buy(msg.sender, seller, id, price);
     }
+
 
     function withdraw() public {
         require(msg.sender == owner, "Notification: You are not the owner");
         payable(owner).transfer(address(this).balance);
     }
 
+    function net_pay(address collection, uint256 id) public view returns (uint256) {
+        uint256 price = _offers[collection][id];
+        return price + compute_fee(price);
+    }
+
+    function getPrice(address collection, uint256 id) external view returns (uint256) {
+        return _offers[collection][id];
+    }
+
     function getBalance() external view returns (uint256) {
         return  address(this).balance;
+    }
+
+    function compute_fee(uint256 price) private pure returns (uint256) {
+        return price * fee / 100;
     }
 
 }
